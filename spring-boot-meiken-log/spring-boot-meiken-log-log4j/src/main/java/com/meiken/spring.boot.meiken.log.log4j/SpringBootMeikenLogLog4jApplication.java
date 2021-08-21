5c94a0b67bc8ed809f6f787076369824576bcf01
Meiken <aluo_fei@163.com>
2021-08-22 03:10:46 +0800

    log

---
+++ spring-boot-meiken-log/spring-boot-meiken-log-log4j/src/main/java/com/meiken/spring.boot.meiken.log.log4j/SpringBootMeikenLogLog4jApplication.java
@@ -0,0 +1,13 @@
+package com.meiken.spring.boot.meiken.log.log4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMeikenLogLog4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMeikenLogLog4jApplication.class, args);
+    }
+
+}
