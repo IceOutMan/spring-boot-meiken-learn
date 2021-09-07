dd0aa3eac25a28e8d6669a05ffde1b98b095c017
Meiken <aluo_fei@163.com>
2021-09-07 23:16:54 +0800

    add spring-boot sl4j with logback

---
+++ spring-boot-meiken-log/spring-boot-meiken-log-logback/src/main/java/com/meiken/springbootmeikenloglogback/IndexController.java
@@ -0,0 +1,25 @@
+package com.meiken.springbootmeikenloglogback;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author glf
+ * @Date 2021/9/7
+ */
+@RequestMapping(value = "/index")
+@RestController
+@Slf4j
+public class IndexController {
+
+    @GetMapping("/ok")
+    public void logOk(){
+        log.debug("debug");
+        log.info("info");
+        log.warn("warn");
+        log.error("error");
+    }
+
+}
