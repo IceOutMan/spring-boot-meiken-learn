0a3ae12230733b89c8f6232170fb9a2f91543002
IceOutMan <aluo_fei@163.com>
2022-09-06 19:03:45 +0800

    add: thymeleaf expression

---
+++ spring-boot-meiken-template-engine/spring-boot-meiken-template-thymeleaf/src/main/java/com/meiken/springbootmeikentemplatethymeleaf/config/Person.java
@@ -0,0 +1,10 @@
+package com.meiken.springbootmeikentemplatethymeleaf.config;
+
+import lombok.Data;
+
+@Data
+public class Person {
+    private Integer age;
+    private Integer sex;
+    private String name;
+}
