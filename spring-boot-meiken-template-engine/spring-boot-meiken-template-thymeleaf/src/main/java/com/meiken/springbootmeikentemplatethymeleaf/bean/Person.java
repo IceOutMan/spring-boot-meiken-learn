9bc89f85e1d4b774eb6a3194d660b24b73df2e88
IceOutMan <ainewbeginglf@gmail.com>
2023-01-07 19:23:48 +0800

    template
    template

---
+++ spring-boot-meiken-template-engine/spring-boot-meiken-template-thymeleaf/src/main/java/com/meiken/springbootmeikentemplatethymeleaf/bean/Person.java
@@ -0,0 +1,13 @@
+package com.meiken.springbootmeikentemplatethymeleaf.bean;
+
+import lombok.Data;
+
+/**
+ * @Author glf
+ * @Date 2022/8/29
+ */
+@Data
+public class Person {
+    private String name;
+    private Integer age;
+}
