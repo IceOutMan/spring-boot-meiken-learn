f547f3512a29091fd0162ffee36757cd7dc0f07e
IceOutMan <aluo_fei@163.com>
2023-02-14 14:08:10 +0800

    add: zookeeper

---
+++ spring-boot-meiken-zookeeper/src/test/java/com/meiken/springbootmeikenzookeeper/SpringBootMeikenZookeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meiken.springbootmeikenzookeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMeikenZookeeperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
