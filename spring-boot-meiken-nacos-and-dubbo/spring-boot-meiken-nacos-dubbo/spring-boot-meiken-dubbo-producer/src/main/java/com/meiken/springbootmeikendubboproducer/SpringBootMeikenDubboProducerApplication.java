1b18615f80e23afb0ec19f26018d71f47ddd7a20
IceOutMan <aluo_fei@163.com>
2023-02-14 13:52:47 +0800

    refactor: mv file
    1

---
+++ spring-boot-meiken-nacos-and-dubbo/spring-boot-meiken-nacos-dubbo/spring-boot-meiken-dubbo-producer/src/main/java/com/meiken/springbootmeikendubboproducer/SpringBootMeikenDubboProducerApplication.java
@@ -0,0 +1,19 @@
+package com.meiken.springbootmeikendubboproducer;
+
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableDubbo
+@NacosPropertySource(dataId = "spring-boot-meiken-dubbo-producer", autoRefreshed = true)
+//@EnableNacosConfig
+@SpringBootApplication
+public class SpringBootMeikenDubboProducerApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMeikenDubboProducerApplication.class, args);
+    }
+
+}
