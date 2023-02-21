9eaaf466095956bd79b3406832e5fa1239d0f04b
IceOutMan <ainewbeginglf@gmail.com>
2023-02-21 22:17:25 +0800

    add: dubbo

--- spring-boot-meiken-nacos-dubbo/spring-boot-meiken-dubbo-producer/src/main/java/com/meiken/springbootmeikendubboproducer/SpringBootMeikenDubboProducerApplication.java
+++ spring-boot-meiken-nacos-dubbo/spring-boot-meiken-dubbo-producer/src/main/java/com/meiken/springbootmeikendubboproducer/SpringBootMeikenDubboProducerApplication.java
@@ -7,8 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @EnableDubbo
-@NacosPropertySource(dataId = "spring-boot-meiken-dubbo-producer", autoRefreshed = true)
-//@EnableNacosConfig
+// @NacosPropertySource(dataId = "spring-boot-meiken-dubbo-producer", autoRefreshed = true)
+// @EnableNacosConfig
 @SpringBootApplication
 public class SpringBootMeikenDubboProducerApplication {
 
--- spring-boot-meiken-redis/spring-boot-meiken-redis-redisson/src/main/java/com/meiken/springbootmeikenredisredisson/controller/TestController.java
+++ spring-boot-meiken-redis/spring-boot-meiken-redis-redisson/src/main/java/com/meiken/springbootmeikenredisredisson/controller/TestController.java
@@ -5,6 +5,7 @@ import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.concurrent.TimeUnit;
 
