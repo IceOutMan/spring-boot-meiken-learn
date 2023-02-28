f721b9488e53d9d3e6b7e27274d09808c5aeee6b
IceOutMan <aluo_fei@163.com>
2023-02-28 16:22:34 +0800

    mongo
    mongo

---
+++ spring-boot-meiken-mongo/src/main/java/com/meiken/SpringBootMeikenMongoApplication.java
@@ -0,0 +1,13 @@
+package com.meiken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMeikenMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMeikenMongoApplication.class, args);
+    }
+
+}
