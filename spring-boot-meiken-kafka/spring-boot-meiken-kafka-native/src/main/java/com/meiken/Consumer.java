e402697915822961460a1251a0cf892e73781d8d
IceOutMan <ainewbeginglf@gmail.com>
2023-08-16 21:15:24 +0800

    add: kafka

---
+++ spring-boot-meiken-kafka/spring-boot-meiken-kafka-native/src/main/java/com/meiken/Consumer.java
@@ -0,0 +1,10 @@
+package com.meiken;
+
+/**
+ * @Author glf
+ * @Date 2023/7/17
+ */
+public class Consumer {
+    public static void main(String[] args) {
+    }
+}
---
+++ spring-boot-meiken-kafka/spring-boot-meiken-kafka-native/src/main/java/com/meiken/Producer.java
@@ -0,0 +1,11 @@
+package com.meiken;
+
+/**
+ * @Author glf
+ * @Date 2023/7/17
+ */
+public class Producer {
+    public static void main(String[] args) {
+
+    }
+}
