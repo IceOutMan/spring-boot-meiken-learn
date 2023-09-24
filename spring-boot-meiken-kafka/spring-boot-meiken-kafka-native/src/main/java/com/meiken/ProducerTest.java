1a770020e53d916d6401167a3849edc64bc2c2e9
IceOutMan <ainewbeginglf@gmail.com>
2023-09-24 23:45:24 +0800

    add: kafka

--- spring-boot-meiken-kafka/spring-boot-meiken-kafka-native/src/main/java/com/meiken/ProducerTest.java
+++ spring-boot-meiken-kafka/spring-boot-meiken-kafka-native/src/main/java/com/meiken/ProducerTest.java
@@ -1,6 +1,13 @@
 package com.meiken;
 
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
 
 /**
  * @Author glf
@@ -9,18 +16,19 @@ package com.meiken;
 public class ProducerTest {
     private final static String TOPIC = "MK_TOPIC";
     public static void main(String[] args) {
-//
-//        Properties props = new Properties();
-//        props.put("bootstrap.servers", "localhost:9092");
-//        props.put("acks", "all");
-//        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-//        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-//
-//        new
-////        Producer<String, String> producer = new KafkaProducer(props);
-////        for(int i = 0; i < 100; i++)
-////            producer.send(new ProducerRecord<String, String>("my-topic", Integer.toString(i), Integer.toString(i)));
-//
-////        producer.close();
+
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("acks", "all");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer(props);
+        for(int i = 0; i < 100; i++){
+            producer.send(new ProducerRecord<String, String>("my-topic", Integer.toString(i), Integer.toString(i)));
+        }
+
+        producer.close();
+
     }
 }
