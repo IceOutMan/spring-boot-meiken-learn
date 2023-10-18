afc120459bd9fd78c78e5ff7502c40329cb41ddc
meiken.gu <aluo_fei@163.com>
2023-10-18 11:06:06 +0800

    modify: complete nacos use

--- spring-boot-meiken-zookeeper-nacos-dubbo/spring-boot-meiken-nacos-and-dubbo/spring-boot-meiken-nacos/src/main/java/com/meiken/springbootmeikennacos/controller/TestController.java
+++ spring-boot-meiken-zookeeper-nacos-dubbo/spring-boot-meiken-nacos-and-dubbo/spring-boot-meiken-nacos/src/main/java/com/meiken/springbootmeikennacos/controller/TestController.java
@@ -1,11 +1,10 @@
 package com.meiken.springbootmeikennacos.controller;
 
 import com.alibaba.nacos.api.config.annotation.NacosValue;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.*;
-
-import javax.sql.DataSource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Author glf
@@ -18,24 +17,10 @@ public class TestController {
     @NacosValue(value = "${nacosTestField:isNull}", autoRefreshed = true)
     private String nacosTestField;
 
-
-    @Autowired
-    private DataSource dataSource;
-
     @GetMapping("/test")
     @ResponseBody
     public String test() {
         return nacosTestField;
     }
 
-
-    @GetMapping("/datasourceConfig")
-    @ResponseBody
-    public String dataSourceConfig() {
-        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-
-        int rowCount = jdbcTemplate.queryForObject("select count(*) from Teacher", Integer.class);
-
-        return rowCount + "";
-    }
 }
