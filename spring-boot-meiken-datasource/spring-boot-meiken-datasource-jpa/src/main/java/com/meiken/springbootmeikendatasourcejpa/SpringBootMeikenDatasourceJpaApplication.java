380c259da9c75eda27fe168a4732d69a30a6f836
meiken.gu <aluo_fei@163.com>
2023-11-17 19:33:55 +0800

    add: spring boot jpa

---
+++ spring-boot-meiken-datasource/spring-boot-meiken-datasource-jpa/src/main/java/com/meiken/springbootmeikendatasourcejpa/SpringBootMeikenDatasourceJpaApplication.java
@@ -0,0 +1,13 @@
+package com.meiken.springbootmeikendatasourcejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMeikenDatasourceJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMeikenDatasourceJpaApplication.class, args);
+    }
+
+}
